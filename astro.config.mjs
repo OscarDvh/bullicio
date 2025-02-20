2bca9b0d2c52aafc7b08c8d34cae39063630201b
OscarDvh <oscardanielvh@gmail.com>
2025-02-20 14:49:08 -0600

    config astro.config to deploy in render

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'static', // Asegurar que sea "static"
+  build: {
+    outDir: 'dist', // Asegurar que "dist" es el directorio de salida
+  },
+});
